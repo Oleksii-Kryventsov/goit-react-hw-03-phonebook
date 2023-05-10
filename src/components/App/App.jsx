933f1f27283c2ca6ed36a7f72ba61aa733dbd0e1
Oleksii-Kryventsov <kryventsov@gmail.com>
2023-05-11 00:48:52 +0300

    start files

---
+++ src/components/App/App.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import { ContactForm } from '../ContactForm';
+import { Filter } from '../Filter';
+import { ContactList } from '../ContactList';
+import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix';
+import dataContacts from '../../components/data.json';
+import {Container} from './App.styled'
+
+
+export class App extends Component {
+
+  state = {
+    contacts: dataContacts,
+    filter: '',
+  };
+
+  addContact = (data) => {
+
+    if (this.state.contacts.filter(contact => contact.name === data.name).length > 0) {
+      Notify.warning(`${data.name} is in contacts`)
+      return
+    }
+
+    const id = nanoid();
+    const contact = { id: id, name: data.name, number: data.number };
+    const contacts = [contact, ...this.state.contacts];
+
+    this.setState({ contacts: contacts });
+  };  
+
+  deleteContact = (e) => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => e !== contact.id)
+    }))
+  };
+
+
+
+  changeFilter = (event) => {
+    this.setState({ filter: event.currentTarget.value })
+  };
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact => contact.name.toLowerCase().includes(normalizedFilter));
+  }
+  
+  render() { 
+
+    const visibleContacts = this.getVisibleContacts();
+
+    return (
+      <Container>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+
+        <h2>Contacts</h2>
+        <Filter
+          filter={this.state.filter}
+          onChange={this.changeFilter}
+        />
+        <ContactList
+          contacts={visibleContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </Container>
+    )
+  }
+};
